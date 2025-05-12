61ca7fb2bd8e87c0566e41432f5baf02cfb67554
Mohanguptakoduru <mohanguptakoduru@gmail.com>
2025-05-12 21:06:39 +0530

    Updated question1 with recent changes

--- question1/index.js
+++ question1/index.js
@@ -1,11 +1,19 @@
 // question1/index.js
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const stockRoutes = require('./routes/stockRoutes');
 
+app.use(cors()); 
+
+
+
+
 app.use(express.json());
+
 app.use('/stocks', stockRoutes);
 app.use('/stockcorrelation', stockRoutes);
+
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
